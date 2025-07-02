a99c371d0639fd918f18412a18dbd742be62d76a
eolannw <eolann.whyte@stryker.com>
2025-07-03 01:27:14 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// IMPORTANT: DO NOT COMMIT THIS FILE TO A PUBLIC GITHUB REPOSITORY.
+// This file contains sensitive URLs and should be kept private.
+
+const CONFIG = {
+    // Flow for creating a NEW request
+    REQUEST_CREATE_URL: "https://prod-135.westus.logic.azure.com:443/workflows/075b978523814f56951805720dc2da6d/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=7fsONcoc2c82EmQGTmubH_9PUgrWGRZz833KgAThavg",
+    
+    // Flow for looking up existing requests
+    REQUEST_LOOKUP_URL: "https://prod-139.westus.logic.azure.com:443/workflows/939c3e7c315b43b8b12300ea476dbbd2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-uqONv3t7aw9Kwy8wu32OMt-RO1EAlNkjBmDslP60s",
+    
+    // Flow for updating a request
+    REQUEST_UPDATE_URL: "https://prod-188.westus.logic.azure.com:443/workflows/13af96bdb60f4199856014b64e9f3188/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=qOrIZep_-9gmB5sMHFB6t1o2QoRgxXUCPYqXn9zMTUM"
+};

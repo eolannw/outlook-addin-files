37ccbd8342591bbd68823a3cfcfeb7d865bb3d50
eolannw <eolann.whyte@stryker.com>
2025-11-30 19:35:56 +0100

    Update API URLs in config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 
 const CONFIG = {
     // Flow for creating a NEW request
-    REQUEST_CREATE_URL: "https://prod-135.westus.logic.azure.com:443/workflows/075b978523814f56951805720dc2da6d/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=7fsONcoc2c82EmQGTmubH_9PUgrWGRZz833KgAThavg",
+    REQUEST_CREATE_URL: "https://default4e9dbbfb394a4583881053f81f819e.3b.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/075b978523814f56951805720dc2da6d/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=E-_KbenSgz9vTZF2ndPxJArxcKDcyPEdvlObvq6Wsuw",
     
     // Flow for looking up existing requests
-    REQUEST_LOOKUP_URL: "https://prod-139.westus.logic.azure.com:443/workflows/939c3e7c315b43b8b12300ea476dbbd2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Y-uqONv3t7aw9Kwy8wu32OMt-RO1EAlNkjBmDslP60s",
+    REQUEST_LOOKUP_URL: "https://default4e9dbbfb394a4583881053f81f819e.3b.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/939c3e7c315b43b8b12300ea476dbbd2/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=E8lisU2mBVpHeuzjUn_1zCzhHYj2MibEADWgeR16Pyw",
     
     // Flow for updating a request
-    REQUEST_UPDATE_URL: "https://prod-188.westus.logic.azure.com:443/workflows/13af96bdb60f4199856014b64e9f3188/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=qOrIZep_-9gmB5sMHFB6t1o2QoRgxXUCPYqXn9zMTUM"
+    REQUEST_UPDATE_URL: "https://default4e9dbbfb394a4583881053f81f819e.3b.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/13af96bdb60f4199856014b64e9f3188/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5JWGdhS0o9TIhjQMT_kqyPbJqSddn2QDE5JssnfxHTw"
 };
